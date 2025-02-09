fdb40b5f07a9803feacd77a1620db1a2a03840d3
green3diamond <z.dimitrov308@hotmail.com>
2025-02-09 14:09:34 +0200

    gitignore, next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    // webpack: (config, { dev, isServer }) => {
-    //   // This is only needed for development builds
-    //   if (dev && !isServer) {
-    //     config.experiments = {
-    //       ...config.experiments,
-    //       topLevelAwait: true,
-    //       layers: true  // Enable the layers experiment
-    //     }
-    //   }
-    //   return config
-    // },
 }
   export default nextConfig
 
